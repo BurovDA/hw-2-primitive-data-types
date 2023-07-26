13e5f932f1c08bf5561a1f5a433ad6c6517e0154
Burov Dmitry <BurovDA@list.ru>
2023-07-26 09:49:56 +0300

    Update commit

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ public class Main {
         int milePrice = 20; // Количество рублей для получения одной бонусной мили
 
         System.out.println((ticketPrice / milePrice) + " Миль(и) начислено за купленный билет");
-        int mileBonus = 632;
+
+        int mileBonus = 632; // Количество бесплатных миль начисленных за купленный билет
 
     }
 }
